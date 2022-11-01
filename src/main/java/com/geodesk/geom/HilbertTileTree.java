a2765a55a801e2a02f98b16abdc58b2809725657
deseo <martindesmond+clarisma@gmail.com>
2022-11-01 14:14:04 +0100

    restored clobbered third-party notices

--- src/main/java/com/geodesk/geom/HilbertTileTree.java
+++ src/main/java/com/geodesk/geom/HilbertTileTree.java
@@ -5,6 +5,30 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+/*
+ *
+ * This class is based on flatbush (https://github.com/mourner/flatbush)
+ * by Volodymyr Agafonkin. The original work is licensed as follows:
+ *
+ * ISC License
+ *
+ * Copyright (c) 2018, Volodymyr Agafonkin
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ * (https://github.com/mourner/flatbush/blob/master/LICENSE)
+ */
+
 package com.geodesk.geom;
 
 import java.util.ArrayList;
