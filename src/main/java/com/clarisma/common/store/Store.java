clarisma/geodesk

commit 7df47bc147d33e18378b324bebdcdd298b965096

notes & cleanup
- FeatureStore.codeFromString() now returns 0 for ""

src/main/java/com/clarisma/common/store/Store.java
@@ -859,6 +859,9 @@ public abstract class Store
      */
     // TODO: order matters! see notes above re downloading tile: index entry
     //  must be written last, else we'll get a race condition
+    //   This issue is more pertinent for commit(): must ensure proper order
+    //   of blocks being written; the order of calls to force(), however,
+    //   should not matter
 
     private void syncSegments(IntSet affectedSegments)
     {
@@ -1031,8 +1034,13 @@ public abstract class Store
         saveJournal();
 
         // Copy the contents of all journaled blocks into their segments
-        // (each semgent is a 1-GB MappedByteBuffer); track which segments
+        // (each segment is a 1-GB MappedByteBuffer); track which segments
         // have been modified
+
+        // TODO: order matters! Possible race condition where index blocks
+        //  are written before tile contents -- make sure to write blocks
+        //  that are part of metadata *last*
+
         MutableIntSet affectedSegments = new IntHashSet();
         for(TransactionBlock block: transactionBlocks.values())
         {

src/main/java/com/geodesk/feature/store/FeatureStore.java
@@ -33,8 +33,21 @@ public class FeatureStore extends BlobStore
 {
     private int minZoom;
     private int zoomSteps;
+    /**
+     * A mapping of strings to their string-table code.
+     * TODO: Currently, empty string is *not* in this map (since 0 means
+     *  "this key is not a global string"); should we revise this definition?
+     *
+     */
     private ObjectIntMap<String> stringsToCodes;
+    /**
+     * The global string table. Entry 0 is always an empty string ("")
+     */
     private String[] codesToStrings;
+    /**
+     * A mapping of indexed keys (global-string code) to their index category
+     * (1-based; range 1 to 30)
+     */
     private IntIntMap keysToCategories;
     private ExecutorService executor;
     private MatcherCompiler matchers;
@@ -65,6 +78,9 @@ public class FeatureStore extends BlobStore
 
         readStringTable();
         readIndexSchema();
+
+        // Querying is enabled explicitly by the FeatureLibrary class, which
+        // serves as the front-end of the API
         // enableQueries();
         int zoomLevels = zoomLevels();
         minZoom = ZoomLevels.minZoom(zoomLevels);
@@ -101,10 +117,16 @@ public class FeatureStore extends BlobStore
         codesToStrings = new String[count + 1];
         codesToStrings[0] = "";
 
+        // TODO: does this make sense? Doesn't the map already have a load factor?
+        //  (But higher capacity may reduce hash collisions and make lookup more efficient)
         MutableObjectIntMap<String> stringMap =
             new ObjectIntHashMap<>(count + (count >> 1));
 
-        for (int i = 1; i <= count; i++)
+        // TODO: Check if we need to have "" in this map (currently not included)
+        //  1/11/23: Changed to include "" as entry 0
+        //  (to determine if a key is not in the table, must supply -1 as default)
+
+        for (int i = 0; i <= count; i++)
         {
             String s = reader.readString();
             codesToStrings[i] = s;
@@ -145,6 +167,12 @@ public class FeatureStore extends BlobStore
         return baseMapping.getInt(TILE_INDEX_PTR_OFS) + TILE_INDEX_PTR_OFS;
     }
 
+    // TODO: this is hacky, needed for TileCatalog in GOL Tool
+    public static int tileIndexPointer(ByteBuffer buf)
+    {
+        return buf.getInt(TILE_INDEX_PTR_OFS) + TILE_INDEX_PTR_OFS;
+    }
+
     // TODO: consolidate with getIndexEntry
     public int tilePage(int tip)
     {
@@ -164,9 +192,16 @@ public class FeatureStore extends BlobStore
         }
     }
 
+    /**
+     * Returns the global string code for a given string.
+     *
+     * @param s     the string to look up in the GST
+     * @return      the string code (range 0 -  65,535 inclusive), or -1
+     *              if the string is not in the GST
+     */
     public int codeFromString(String s)
     {
-        return stringsToCodes.get(s);
+        return stringsToCodes.getIfAbsent(s, -1);
     }
 
     public GeometryFactory geometryFactory()
@@ -271,5 +306,20 @@ public class FeatureStore extends BlobStore
             indexedKeys.put(codesToStrings[k], category);
         });
         return indexedKeys;
+    }
+
+    public ObjectIntMap<String> stringsToCodes()
+    {
+        return stringsToCodes;
+    }
+
+    // TODO: Use only for build; does not use transaction
+    //  Move to Compiler?
+    public synchronized int createTile(int tip, int size)
+    {
+        int page = allocateBlob(size);
+        int p = tileIndexPointer() + tip * 4;
+        baseMapping.putInt(p, page << 1);
+        return page;
     }
 }

pom.xml
@@ -4,7 +4,7 @@
 
   <groupId>com.geodesk</groupId>
   <artifactId>geodesk</artifactId>
-  <version>0.1.4</version>
+  <version>0.1.5-SNAPSHOT</version>
   <packaging>jar</packaging>
 
   <name>GeoDesk</name>

src/main/java/com/clarisma/common/store/BlobStore.java
@@ -126,6 +126,9 @@ public class BlobStore extends Store
      */
     protected boolean isEmpty()
     {
+        // TODO: Use a different definition of "empty";
+        //  better candidate: Dataset Revision == 0
+
         return baseMapping.getInt(INDEX_PTR_OFS) == 0;      // TODO
     }
 

src/main/java/com/clarisma/common/store/BlobStoreConstants.java
@@ -34,7 +34,7 @@ public class BlobStoreConstants
      */
     public static final int VERSION_OFS = 4;
     public static final int TIMESTAMP_OFS = 8;
-    public static final int INDEX_PTR_OFS = 44;
+    public static final int INDEX_PTR_OFS = 44; // TODO: not part of blobStore
 
     // TODO: GUID_OFS
     //  PROPERTIES_PTR_OFS

src/main/java/com/clarisma/common/util/Bytes.java
@@ -129,14 +129,7 @@ public class Bytes
 
         byte[] chars = new byte[len];
         buf.get(p, chars);
-        try
-        {
-            return new String(chars, StandardCharsets.UTF_8);
-        }
-        catch (Exception ex)
-        {
-            throw new RuntimeException("Unable to decode string.", ex);
-        }
+        return new String(chars, StandardCharsets.UTF_8);
     }
 
     /**

src/main/java/com/geodesk/feature/match/MatcherCoder.java
@@ -1326,6 +1326,8 @@ public class MatcherCoder extends ExpressionCoder
 	 *   type), and this tag is the last, the code branches to selector_success
 	 * Otherwise, the code continues. If there are further clauses of the same
 	 * type, $pos is moved to the next key in the table.
+	 *
+	 * Required fields: clause
 	 */
 	private void matchClause()
 	{

src/main/java/com/geodesk/feature/match/RoleMatcher.java
@@ -16,6 +16,7 @@ public class RoleMatcher extends Matcher
     public RoleMatcher(FeatureStore store, String role)
     {
         roleCode = store.codeFromString(role);
+        // TODO: empty role should be 0
         assert roleCode != 0;
     }
 

commit 35b7aa888489f03d4a2d008bb5d7039cae8ed5c0

fix: string table loading (regression)
(regression introduced in 7df47bc1)

src/main/java/com/clarisma/common/store/Store.java
@@ -937,7 +937,8 @@ public abstract class Store
         }
         catch(IOException ex)
         {
-            throw new StoreException("Error while closing file", path, ex);
+            throw new StoreException("Error while closing file (%s)"
+                .formatted(ex.getMessage()), path, ex);
         }
         finally
         {

src/main/java/com/geodesk/feature/store/FeatureStore.java
@@ -121,12 +121,13 @@ public class FeatureStore extends BlobStore
         //  (But higher capacity may reduce hash collisions and make lookup more efficient)
         MutableObjectIntMap<String> stringMap =
             new ObjectIntHashMap<>(count + (count >> 1));
+        stringMap.put("", 0);
 
         // TODO: Check if we need to have "" in this map (currently not included)
         //  1/11/23: Changed to include "" as entry 0
         //  (to determine if a key is not in the table, must supply -1 as default)
 
-        for (int i = 0; i <= count; i++)
+        for (int i = 1; i <= count; i++)
         {
             String s = reader.readString();
             codesToStrings[i] = s;